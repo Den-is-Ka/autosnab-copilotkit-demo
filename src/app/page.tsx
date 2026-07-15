8c230c0c7c042544b6f9d6531fb857baa5b1cd3b
Den-is-Ka <d_k81@mail.ru>
2026-07-15 13:08:30 +0500

    LLM не пустил по региону, пишем дальше

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import { CopilotSidebar } from "@copilotkit/react-core/v2";
+
 import { ApplicationsDashboard } from "@/components/applications_dashboard";
 
 export default function Home() {
-  return <ApplicationsDashboard />;
+  return (
+    <>
+      <ApplicationsDashboard />
+
+      <CopilotSidebar
+        agentId="default"
+        labels={{
+          modalHeaderTitle: "AI-ассистент АвтоСнаб",
+        }}
+      />
+    </>
+  );
 }
